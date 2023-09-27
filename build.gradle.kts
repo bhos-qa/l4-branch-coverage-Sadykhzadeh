8018cdbeafaa83d881ee17ca12ddb9c3d973b3a9
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2023-09-27 21:35:31 +0400

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    jacoco
     id("org.sonarqube") version "4.3.1.3277"
 }
 
@@ -9,8 +10,39 @@ sonar {
         property("sonar.projectKey", "bhos-qa_l4-branch-coverage-Sadykhzadeh")
         property("sonar.organization", "bhos-qa")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.scm.forceReloadAll", "true")
+        property("sonar.java.coveragePlugin", "jacoco")
+        property("sonar.jacoco.reportPath", "build/JaCoCoTest/test")
+        property("sonar.junit.reportsPath", "build/test-results/test")
     }
 }
+
+
+jacoco {
+    toolVersion = "0.8.8"
+    reportsDirectory.set(layout.buildDirectory.dir("reportsJaCoCo"))
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+jacoco {
+    toolVersion = "0.8.9"
+    reportsDirectory.set(layout.buildDirectory.dir("JaCoCoTest"))
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+
 
 group = "one.azer"
 version = "1.0-SNAPSHOT"
